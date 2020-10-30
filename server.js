3c6419ac199111c98af2ae876ce963b47d1ebdca
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-10-31 00:25:34 +0200

    fix 6 for heroku c2c

--- server.js
+++ server.js
@@ -6,21 +6,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/dist/ngimal')));
 
-app.use(function (req, res, next) {
-    var allowedOrigins = ['https://mysterious-reef.herokuapp.com/'];
-    var origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS', 'PUT');
-    res.header('Access-Control-Allow-Credentials', true);
-    return next();
-});
-
-app.use('/api/', 'https://mysterious-reef.herokuapp.com/');
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/ngimal/index.html'));
 });
