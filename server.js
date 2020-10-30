e69f7de0629aaf8a0762a09ba8e5c57ed1560db0
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-10-31 00:53:31 +0200

    fix 8 for heroku c2c

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const http = require('http')
 const path = require('path');
+var cors = require('cors');
 
 const app = express();
 
-app.use('/api', 'https://mysterious-reef.herokuapp.com')
+app.use(cors());
+
+app.options('*', cors());
 
 app.use(express.static(path.join(__dirname, '/dist/ngimal')));
+
+app.get('/api', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/ngimal/index.html'));
