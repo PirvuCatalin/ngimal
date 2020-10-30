afe549d83607f2fa32c4c2c04752cf5f16969041
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-10-31 01:32:23 +0200

    fix 14

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const http = require('http')
 const path = require('path');
-var cors = require('cors');
 
 const app = express();
 
-app.use(cors());
-
-app.options('*', cors());
+app.use(express.static(path.join(__dirname, '/dist/ngimal')));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
