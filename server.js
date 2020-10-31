bad340ab6179111c68a1eea92103bcd3438fd676
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-10-31 09:16:03 +0200

    final good version

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
+const app = express();
 
-var app = express();
-app.use(bodyParser.json());
+// add this code
+const whitelist = ['https://mysterious-reef.herokuapp.com']; // list of allow domain
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/ngimal";
-app.use(express.static(distDir));
-
-const request = require('request');
-
-app.get("/api/get_thingy", function(req, res) {
-    
-    request('https://mysterious-reef.herokuapp.com/get_thingy', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage. 
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
         }
-    });
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
+
+app.use(express.static('./dist/ngimal'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/ngimal/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
